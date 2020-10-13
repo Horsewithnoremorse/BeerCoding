cb57327a19581550ff004ca4cfd4d0bbb340324c
Алексей Мильто <miltoalvit@gmail.com>
2020-10-13 23:00:10 +0300

    Допилил несколько загадок

--- 2nd_Beer_Coding/Program.cs
+++ 2nd_Beer_Coding/Program.cs
@@ -12,7 +12,82 @@ namespace _2nd_Beer_Coding
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Привет, ты запустил мою прогу для загадок, их будет 10шт.");
+            Console.WriteLine("Отвечай одним словом с большой буквы. После каждой загадки программа скажет правильный был ответ или нет");
+            Console.WriteLine("После каждой загадки программа скажет правильный был ответ или нет. Нажми любую клавишу что бы начать");
+            Console.ReadKey();
+            int counter = 0;
+
+            Console.WriteLine("Загадка 1. Вы сидите в самолете, спереди вас лошадь, сзади автомобиль. Что это за место");
+            string Input = Console.ReadLine();
+            if (Input == "Карусель" || Input == "карусель" || Input == "КАРУСЕЛЬ")
+            {
+                Console.WriteLine("Правильно");
+                counter++;
+            }
+            else
+            {
+                Console.WriteLine("Не угадал. Ничего, повезет дальше");
+            }
+
+            Console.WriteLine("Загадка 2. С когтями, а не птица, летит и матерится.");
+            Input = Console.ReadLine();
+            if (Input == "Электромонтер" || Input == "'электромонтер" || Input == "ЭЛЕКТРОМОНТЕР")
+            {
+                Console.WriteLine("Правильно");
+                counter++;
+            }
+            else
+            {
+                Console.WriteLine("Не угадал. Ничего, дальше проще будут!");
+            }
+
+            Console.WriteLine("Загадка 3. Что может в одно и то же время стоять и ходить, висеть и стоять, ходит и лежать?");
+            string Result = (Input == "Часы" || Input == "часы" || Input == "ЧАСЫ") ? "Правильно" : "Ты ошибся дружочек-пирожочек";
+            if (Result == "Правильно") 
+            {
+                counter++;           
+            }
+
+            Console.WriteLine("Загадка 4. В каком городе спрятались мужское имя и сторона света?");
+            Input = Console.ReadLine();
+            if (Input == "Владивосток" || Input == "владивосток" || Input == "ВЛАДИВОСТОК")
+            {
+                Console.WriteLine("Правильно");
+                counter++;
+            }
+            else 
+            {
+                Console.WriteLine("Не угадал");
+            }
+
+            Console.WriteLine("Загадка 5. Человек при жизни получает это трижды: два раза абсолютно бесплатно, на третий раз ему приходится за это платить. Что это?");
+            Input = Console.ReadLine();
+            if (Input == "Зубы" || Input == "зубы" || Input == "ЗУБЫ")
+            {
+                Console.WriteLine("Правильно");
+                counter++;
+            }
+            else
+            {
+                Console.WriteLine("Не угадал");
+            }
+
+            Console.WriteLine("Загадка 6. Какой знак нужно поставить между 6 и 7, чтобы результат оказался меньше 7 и больше 6?");
+            Input = Console.ReadLine();
+            if (Input == "Запятая" || Input == "запятая" || Input == "Запятую" || Input == "запятую")
+            {
+                Console.WriteLine("Правильно");
+                counter++;
+            }
+            else
+            {
+                Console.WriteLine("Не угадал");
+            }
+
+
+
+            Console.WriteLine($"Вот и все загадки, а вот и твой результат {counter}/10.");
         }
     }
 }
